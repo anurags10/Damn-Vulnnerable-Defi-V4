0fb54beb8585890c7b834e8fb106ec3851ecd15a
Anurag Singh <iamanuragsingh10@gmail.com>
2025-05-10 20:16:03 +0530

    Added Unstoppable Solution

--- src/unstoppable/UnstoppableVault.sol
+++ src/unstoppable/UnstoppableVault.sol
@@ -81,6 +81,9 @@ contract UnstoppableVault is IERC3156FlashLender, ReentrancyGuard, Owned, ERC462
     {
         if (amount == 0) revert InvalidAmount(0); // fail early
         if (address(asset) != _token) revert UnsupportedCurrency(); // enforce ERC3156 requirement
+
+        // ⚠️ Security check: prevents asset/share mismatch due to direct token transfers or accounting bugs
+        // Vulnerable
         uint256 balanceBefore = totalAssets();
         if (convertToShares(totalSupply) != balanceBefore) revert InvalidBalance(); // enforce ERC4626 requirement
 
